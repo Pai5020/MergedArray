08582f94618b00cd026e4f7990f0b081460b5951
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-05-08 15:26:34 +0530

    modified

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         int i;
-        int[] arr = new int[4];
-        int[] arr2 = new int[4];
+        Integer[] arr = new Integer[4];
+        Integer[] arr2 = new Integer[7];
         Scanner sc = new Scanner(System.in);
-        int size = arr.length;
-        int size1 = arr2.length;
-        Integer[] marray = new Integer[size + size1];
+        Integer[] marray;
 
         System.out.println("Enter array 1 ");
         for (i = 0; i < arr.length; i++) {
@@ -21,10 +18,10 @@ public class Main {
             arr2[i] = sc.nextInt();
         }
         System.out.println("Merged array : ");
-        for (i = 0; i < size ; i++) {
-            marray[i] = arr[i];
-            marray[size+i] = arr2[i];
-        }
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(arr));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(arr2));
+        list1.addAll(list2);
+        marray = list1.toArray(new Integer[0]);
 
         System.out.println(Arrays.toString(marray));
         Arrays.sort(marray, Collections.reverseOrder());
